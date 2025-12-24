65f1c9f97491db401b8eae15805902acb1afcf6e
Abhishek <abhishekak87378@gmail.com>
2025-12-23 22:15:15 -1200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/EMS")
+    await mongoose.connect("mongodb+srv://abhishek4712ak_db_user:KU2c8GAnojKaKSvS@zestdb.zrxg6yb.mongodb.net/zest2k25?retryWrites=true&w=majority&appName=zestdb")
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
