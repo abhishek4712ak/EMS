0b061f44421d1949a8599616288a8ec53819cedf
Abhishek <abhishekak87378@gmail.com>
2026-02-27 16:34:17 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://abhishek4712ak1_db_user:2mBJMySRZVGT9ohw@cluster0.t5tthny.mongodb.net/EMS_DB?appName=Cluster0")
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
